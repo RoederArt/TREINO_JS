add62cc835befea90a0f54ba4ac749646d77d69c
Arthur <arthur_roeder@estudante.sesisenai.org.br>
2026-03-20 12:11:25 -0300

    add - segunda atividade feita

---
+++ scripts/ex02.js
@@ -0,0 +1,11 @@
+//console.log('2');
+
+
+function contagemRegressiva() {
+
+    for(let i=10; i > 0; i--){
+     alert("nosso foguete vai decolar em: " + i);
+    console.log(i);
+    }
+     alert("lançamento bem sucedido!");
+}
