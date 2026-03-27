14a1ab93594eed9215180bd7dcfbf418911469df
Arthur <arthur_roeder@estudante.sesisenai.org.br>
2026-03-27 11:49:18 -0300

    add - sexta atividade feita e 7 atividade adicionada

---
+++ scripts/ex06.js
@@ -0,0 +1,24 @@
+console.log('6');
+
+function vogalConsoante() {
+
+    let letra = prompt("digite um letra: ");
+
+    if (letra == "a" || letra == "A"){
+        alert("vogal");
+    }else if(letra == "e" || letra == "E"){
+        alert("vogal");
+    }else if(letra == "i" || letra == "I"){
+        alert("vogal");
+    }else if(letra == "o" || letra == "O"){
+        alert("vogal");
+    }else if(letra == "u" || letra == "U"){
+        alert("vogal");
+
+    } else {
+
+        alert("consoante");
+
+    }
+
+}
