5ba9833e6f2fc549a3f57c3998efeded76a14a7e
Arthur <arthur_roeder@estudante.sesisenai.org.br>
2026-03-27 12:23:34 -0300

    add - setima atividade completa

---
+++ scripts/ex07.js
@@ -0,0 +1,35 @@
+function saBOR() {
+
+    let tenis = prompt("Sabores disponíveis\n\n         Sabor            Preço \n a = Chocolate     R$ 1,50 \n b = Morango     R$ 2,50 \n c =Creme     R$ 2,50 \n d = Manga     R$ R$ 3,20 \n e = Melancia     R$ 3,40 \n f = Vanilla Ice     R$ 3,00 \n g = Céu Azul     R$ 3,60 \n h = Brownie     R$ 4,00 \n i = Hawaiano     R$ 5,00");
+   
+    switch(tenis){
+        case "a":
+            alert("seu pedido ficou custando a bagatela de R$1,50");
+            break;
+        case "b":
+            alert("seu pedido ficou custando a bagatela de R$2,50");
+            break;
+            case "c":
+            alert("seu pedido ficou custando a bagatela de R$2,50");
+            break;
+            case "d":
+            alert("seu pedido ficou custando a bagatela de R$3,20");
+            break;
+            case "e":
+            alert("seu pedido ficou custando a bagatela de R$3,40");
+            break;
+            case "f":
+            alert("seu pedido ficou custando a bagatela de R$3,00");
+            break;
+            case "g":
+            alert("seu pedido ficou custando a bagatela de R$3,60");
+            break;
+            case "h":
+            alert("seu pedido ficou custando a bagatela de R$4,00");
+            break;
+            case "i":
+            alert("seu pedido ficou custando a bagatela de R$5,00");
+            break;        
+    }
+
+}
