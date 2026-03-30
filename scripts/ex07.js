43688be570b0b329a8fc70eba4dd099787558860
Arthur <arthur_roeder@estudante.sesisenai.org.br>
2026-03-30 11:36:13 -0300

    add - atividade oito finalizada

--- scripts/ex07.js
+++ scripts/ex07.js
@@ -1,3 +1,5 @@
+console.log('7');
+
 function saBOR() {
 
     let tenis = prompt("Sabores disponíveis\n\n         Sabor            Preço \n a = Chocolate     R$ 1,50 \n b = Morango     R$ 2,50 \n c =Creme     R$ 2,50 \n d = Manga     R$ R$ 3,20 \n e = Melancia     R$ 3,40 \n f = Vanilla Ice     R$ 3,00 \n g = Céu Azul     R$ 3,60 \n h = Brownie     R$ 4,00 \n i = Hawaiano     R$ 5,00");
---
+++ scripts/ex08.js
@@ -0,0 +1,23 @@
+console.log('8');
+
+function variaveis() {
+
+    let numero1 = 0;
+    let numero2 = 0;
+
+numero1 = parseInt(prompt("digite um numero: "));
+numero2 = parseInt(prompt("digite um numero: "));
+
+let numero3 = numero1 - numero2;
+
+alert("diferença: " + numero3);
+
+let  numero4 = (numero1 * 2) + (numero2 * 3);
+
+alert("O dobro do primeiro numero somando ao triplo do segundo: " + numero4);
+
+let numero5 = numero1 * numero2;
+
+alert("multiplicação dos dois numeros: " + numero5);
+
+}
